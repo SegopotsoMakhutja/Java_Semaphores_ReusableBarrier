2f231ac109539bb0ac14b1719d2dea7dcfb22449
Segopotso <leketi.s@gmail.com>
2019-05-06 17:17:37 +0200

    run method added, now testing.

--- Hydrogen.java
+++ Hydrogen.java
@@ -5,21 +5,36 @@ public class Hydrogen extends Thread {
 	private static int carbonCounter =0;
 	private int id;
 	private Methane sharedMethane;
-	
+	//private Barrier sharedBarrier;
 	
 	public Hydrogen(Methane methane_obj) {
 		Hydrogen.carbonCounter+=1;
 		id=carbonCounter;
 		this.sharedMethane = methane_obj;
-		
 	}
 	
 	public void run() {
 	    try {
-	    	 // you will need to fix below
-	    	System.out.println("---Group ready for bonding---");			 
-	    	sharedMethane.bond("H"+ this.id);
-	    }
+	    	// you will need to fix below			 
+				sharedMethane.mutex.acquire();
+				sharedMethane.addHydrogen();
+				if(sharedMethane.getHydrogen()>=4 && sharedMethane.getCarbon()>=1)
+				{
+					sharedMethane.hydrogensQ.release(4);
+					sharedMethane.removeHydrogen(4);
+					sharedMethane.carbonQ.release();
+					sharedMethane.removeCarbon(1);
+				}
+				else
+				{
+					sharedMethane.mutex.release();
+				}
+				sharedMethane.hydrogensQ.acquire();
+				System.out.println("---Group ready for bonding---");
+				sharedMethane.bond("H"+ this.id);
+				sharedMethane.barrier.b_wait();
+			
+			}
 	   catch (InterruptedException ex) { /* not handling this  */}
 	    //System.out.println(" ");
 	}
