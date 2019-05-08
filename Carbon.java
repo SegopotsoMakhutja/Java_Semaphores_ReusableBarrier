3b6c8dbb931624be584dd01d044db3515fb6e21d
Segopotso <leketi.s@gmail.com>
2019-05-08 21:04:24 +0200

    cleaned up carbon and added comments

--- Carbon.java
+++ Carbon.java
@@ -1,45 +1,43 @@
+package molecule;
+
 import java.util.concurrent.Semaphore;
 import java.util.Random;
-//package molecule;
 
 public class Carbon extends Thread {
-	
-	private static int carbonCounter =0;
+
+	private static int carbonCounter = 0;
 	private int id;
 	private Methane sharedMethane;
-	//private Semaphore mutex;
 
 	public Carbon(Methane methane_obj) {
-		Carbon.carbonCounter+=1;
-		id=carbonCounter;
-		this.sharedMethane = methane_obj;
-		//this.mutex = new Semaphore(1);
+		Carbon.carbonCounter += 1;
+		id = carbonCounter; // id for the atoms
+		this.sharedMethane = methane_obj; // sharedMethane obj
 	}
-	
+
 	public void run() {
-	    try {	 
-			// you will need to fix below	
-			sharedMethane.mutex.acquire();
-			sharedMethane.addCarbon();
-			if(sharedMethane.getHydrogen()>=4)
+		try {
+			// you will need to fix below
+			sharedMethane.mutex.acquire(); // mutex lock for one thread
+			sharedMethane.addCarbon(); // add carbon atom to the methane molecule
+
+			// check how many atoms at barrier
+			if (sharedMethane.getHydrogen() >= 4)
 			{
 				sharedMethane.hydrogensQ.release(4);
 				sharedMethane.removeHydrogen(4);
 				sharedMethane.carbonQ.release();
 				sharedMethane.removeCarbon(1);
-			}
-			else
+			} 
+			else 
 			{
-				sharedMethane.mutex.acquire();
+				sharedMethane.mutex.acquire(); // unlock mutex
 			}
 			sharedMethane.carbonQ.acquire();
-			sharedMethane.bond("C"+ this.id);  //bond
-			System.out.println("---Group ready for bonding---");
-			sharedMethane.barrier.b_wait();
-			sharedMethane.mutex.release();
-	    }
-	    catch (InterruptedException ex) { /* not handling this  */}
-	   // System.out.println(" ");
+			sharedMethane.bond("C" + this.id); // bond
+			sharedMethane.barrier.b_wait(); 
+			sharedMethane.mutex.release(); // unlock mutex
+		} catch (InterruptedException ex) { /* not handling this */ }
+		// System.out.println(" ");
 	}
-
 }
